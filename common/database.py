44b99a9b977dd26cbcacbbbd6e8e22bade9ae7a2
moon <mool2480@gmail.com>
2023-02-22 10:28:55 +0900

    fix: db정보 변경

--- common/database.py
+++ common/database.py
@@ -4,15 +4,15 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 class Database:
     name = "mysql+pymysql"
-    user = "member"
-    password = "member"
+    user = "root"
+    password = "root"
     host = "localhost"
     db_name = "pilot"
     port = "3306"
 
     def __init__(self):
         database_url = f"{self.name}://{self.user}:{self.password}@{self.host}:{self.port}/{self.db_name}"
-        self.engine = create_engine(database_url)
+        self.engine = create_engine(database_url, echo=True)
 
     def get_session(self):
         session = sessionmaker(bind=self.engine)
@@ -20,5 +20,4 @@ class Database:
             yield db
 
 
-db = Database()
 Base = declarative_base()
