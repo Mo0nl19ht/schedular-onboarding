e5aec155dad8717d7f3d659f0a6253c730d67d08
moon <mool2480@gmail.com>
2023-03-02 15:34:52 +0900

    [#1]:feat: 모든 api에 인가 기능 추가

--- member/controller/user_controller.py
+++ member/controller/user_controller.py
@@ -16,8 +16,8 @@ class UserController:
         self.user_service = user_service
 
         self.router.add_api_route("", self.create, methods=["POST"])
-        self.router.add_api_route("/{login_id}", self.delete, methods=["DELETE"])
-        self.router.add_api_route("/{login_id}", self.update, methods=["put"])
+        self.router.add_api_route("", self.delete, methods=["DELETE"])
+        self.router.add_api_route("", self.update, methods=["put"])
         self.router.add_api_route("", self.find_by_login_id, methods=["get"])
         self.router.add_api_route("/login", self.login, methods=["post"])
 
@@ -27,10 +27,14 @@ class UserController:
     def create(self, member_create_dto: MemberCreateDto):
         return self.user_service.create(member_create_dto)
 
-    def delete(self, login_id: str):
+    def delete(self, login_id: str = Depends(get_current_member)):
         return self.user_service.delete(login_id)
 
-    def update(self, login_id: str, user_update_dto: MemberUpdateDto) -> Jwt:
+    def update(
+        self,
+        user_update_dto: MemberUpdateDto,
+        login_id: str = Depends(get_current_member),
+    ) -> Jwt:
         return self.user_service.update(login_id, user_update_dto)
 
     def find_by_login_id(self, login_id: str = Depends(get_current_member)):
